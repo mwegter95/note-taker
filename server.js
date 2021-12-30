eac498f14b7058b30d163e95a9f2143389ce76cb
hitmaker <mwegter95@gmail.com>
2021-12-29 22:15:04 -0600

    added a get route for *

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
